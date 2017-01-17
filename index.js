68dfc64a92c2463712d10c1ae55ba345767289f4
Josh Nankin <josh@phaxio.com>
2017-01-17 08:57:46 -0600

    try it again

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express()
-var port = process.env.PORT || 5000;
+
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
   res.send('Set the URL of this app to be your receive callback URL in Phaxio! ')
 })
 
-app.listen(port, function () {
-  console.log('Phaxio app listening on port ' + port + '!')
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
